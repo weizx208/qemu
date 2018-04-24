1ec541e918e688da3167686ce129fce31d04d61c
Sai Pavan Boddu <sai.pavan.boddu@xilinx.com>
2018-04-25 00:03:27 +0530

    xilinx_spips: Correct SNOOP_NONE state when flushing the txfifo
    SNOOP_NONE state handle is moved above in the if ladder, as its same
    as SNOOP_STRIPPING during data cycles.
    
    Signed-off-by: Sai Pavan Boddu <saipava@xilinx.com>
    Reviewed-by: Francisco Iglesias <frasse.iglesias@gmail.com>
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>

--- hw/ssi/xilinx_spips.c
+++ hw/ssi/xilinx_spips.c
@@ -618,7 +618,8 @@ static void xilinx_spips_flush_txfifo(XilinxSPIPS *s)
         if (fifo8_is_empty(&s->tx_fifo)) {
             xilinx_spips_update_ixr(s);
             return;
-        } else if (s->snoop_state == SNOOP_STRIPING) {
+        } else if (s->snoop_state == SNOOP_STRIPING ||
+                   s->snoop_state == SNOOP_NONE) {
             for (i = 0; i < num_effective_busses(s); ++i) {
                 if (!fifo8_is_empty(&s->tx_fifo)) {
                     tx_rx[i] = fifo8_pop(&s->tx_fifo);
