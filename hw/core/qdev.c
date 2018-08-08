00d031c135e01a78b1d40fbb13db515136a94c24
Edgar E. Iglesias <edgar.iglesias@xilinx.com>
2018-08-08 21:44:55 +0200

    qdev: reset: Create 6 per device rst control lines
    Create 6 per device rst control lines.
    
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>
    [Luc: squashed with the commit reducing lines from 32 to 6]
    Signed-off-by: Luc Michel <luc.michel@amd.com>

--- hw/core/qdev.c
+++ hw/core/qdev.c
@@ -683,7 +683,7 @@ static void device_initfn(Object *obj)
     qdev_init_gpio_in_named(dev, dc->pwr_cntrl, "pwr_cntrl", 1);
     qdev_init_gpio_in_named(dev, dc->hlt_cntrl, "hlt_cntrl", 1);
     /* Reset control. */
-    qdev_init_gpio_in_named(dev, dc->rst_cntrl, "rst_cntrl", 1);
+    qdev_init_gpio_in_named(dev, dc->rst_cntrl, "rst_cntrl", 6);
 
     QLIST_INIT(&dev->gpios);
     QLIST_INIT(&dev->clocks);
