11a7cbf757a6beacd2d4c78925eac92377331b6a
Jaewook Bae <jaewook.bae@xilinx.com>
2020-01-23 10:33:43 -0800

    zynqmp: intc/redirect: Convert to new register api model
    Replace dep-registers with new registers.
    No functional changes.
    
    Signed-off-by: Jaewook Bae <jaewookb@xilinx.com>
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>

--- hw/intc/xilinx-zynqmp-intc-redirect.c
+++ hw/intc/xilinx-zynqmp-intc-redirect.c
@@ -30,7 +30,8 @@
 #include "qemu/osdep.h"
 #include "qapi/error.h"
 #include "hw/sysbus.h"
-#include "hw/register-dep.h"
+#include "hw/register.h"
+#include "hw/irq.h"
 #include "qemu/bitops.h"
 #include "qemu/log.h"
 
