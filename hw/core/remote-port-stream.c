ac930e6f1bad0a137abd2321a1047a84437cf5aa
Edgar E. Iglesias <edgar.iglesias@xilinx.com>
2020-05-11 12:00:27 +0200

    remote-port: Allow dynamic rp-adaptor detachment
    Allow dynamic rp-adaptor detachment.
    
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>

--- hw/core/remote-port-stream.c
+++ hw/core/remote-port-stream.c
@@ -176,7 +176,7 @@ static void rp_stream_init(Object *obj)
                              OBJ_PROP_LINK_STRONG);
     object_property_add_link(obj, "rp-adaptor0", "remote-port",
                              (Object **)&s->rp,
-                             qdev_prop_allow_set_link_before_realize,
+                             qdev_prop_allow_set_link,
                              OBJ_PROP_LINK_STRONG);
 }
 
