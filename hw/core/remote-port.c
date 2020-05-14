c5eb350f5f840a03988da763d3ec926e0032bed4
Edgar E. Iglesias <edgar.iglesias@xilinx.com>
2020-05-14 17:14:33 +0200

    remote-port: Remove unused rp_sync_vmclock()
    Remove unused rp_sync_vmclock().
    No functional change.
    
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>

--- hw/core/remote-port.c
+++ hw/core/remote-port.c
@@ -227,10 +227,6 @@ RemotePortDynPkt rp_wait_resp(RemotePort *s)
     return s->rspqueue;
 }
 
-void rp_sync_vmclock(RemotePort *s, int64_t lclk, int64_t rclk)
-{
-}
-
 static void rp_cmd_hello(RemotePort *s, struct rp_pkt *pkt)
 {
     s->peer.version = pkt->hello.version;
@@ -326,7 +322,6 @@ static void sync_timer_hit(void *opaque)
 {
     RemotePort *s = REMOTE_PORT(opaque);
     int64_t clk;
-    int64_t rclk;
     RemotePortDynPkt rsp;
 
     clk = rp_normalized_vmclk(s);
@@ -347,11 +342,9 @@ static void sync_timer_hit(void *opaque)
 
     SYNCD(printf("%s: syncing wait for resp %lu\n", s->prefix, clk));
     rsp = rp_wait_resp(s);
-    rclk = rsp.pkt->sync.timestamp;
     rp_dpkt_invalidate(&rsp);
     qemu_mutex_unlock(&s->rsp_mutex);
 
-    rp_sync_vmclock(s, clk, rclk);
     rp_restart_sync_timer_bare(s);
 }
 
