c333cbd7c9d7d9cdfbda58276b35ad86e0e79186
Sai Pavan Boddu <sai.pavan.boddu@amd.com>
2022-11-24 00:31:08 +0530

    target: arm: Add few a78 specific registers
    Add these few registers to make firmware proceed.
    
    Signed-off-by: Sai Pavan Boddu <sai.pavan.boddu@amd.com>
    Signed-off-by: Francisco Iglesias <francisco.iglesias@amd.com>

--- target/arm/tcg/cpu64.c
+++ target/arm/tcg/cpu64.c
@@ -237,6 +237,33 @@ static const ARMCPRegInfo dsu_cp_reginfo[] = {
 };
 #endif
 
+static const ARMCPRegInfo cortex_a78_reginfo[] = {
+    { .name = "CPUECTLR_EL1", .state = ARM_CP_STATE_AA64,
+      .opc0 = 3, .opc1 = 0, .crn = 15, .crm = 1, .opc2 = 4,
+      .access = PL1_RW, .type = ARM_CP_CONST,
+      .resetvalue = 0 },
+    { .name = "CPUPCR_EL3", .state = ARM_CP_STATE_AA64,
+      .opc0 = 3, .opc1 = 6, .crn = 15, .crm = 8, .opc2 = 1,
+      .access = PL3_RW, .type = ARM_CP_CONST,
+      .resetvalue = 0 },
+    { .name = "CPUPMR_EL3", .state = ARM_CP_STATE_AA64,
+      .opc0 = 3, .opc1 = 6, .crn = 15, .crm = 8, .opc2 = 3,
+      .access = PL3_RW, .type = ARM_CP_CONST,
+      .resetvalue = 0 },
+    { .name = "CPUPOR_EL3", .state = ARM_CP_STATE_AA64,
+      .opc0 = 3, .opc1 = 6, .crn = 15, .crm = 8, .opc2 = 2,
+      .access = PL3_RW, .type = ARM_CP_CONST,
+      .resetvalue = 0 },
+    { .name = "CPUPSELR_EL3", .state = ARM_CP_STATE_AA64,
+      .opc0 = 3, .opc1 = 6, .crn = 15, .crm = 8, .opc2 = 0,
+      .access = PL3_RW, .type = ARM_CP_CONST,
+      .resetvalue = 0 },
+    { .name = "CPUACTLR2_EL1", .state = ARM_CP_STATE_AA64,
+      .opc0 = 3, .opc1 = 0, .crn = 15, .crm = 1, .opc2 = 1,
+      .access = PL1_RW, .type = ARM_CP_CONST,
+      .resetvalue = 0 },
+};
+
 static void aarch64_a78_initfn(Object *obj)
 {
     ARMCPU *cpu = ARM_CPU(obj);
@@ -296,6 +323,7 @@ static void aarch64_a78_initfn(Object *obj)
     cpu->gic_vpribits = 5;
     cpu->gic_vprebits = 5;
     define_cortex_a72_a57_a53_cp_reginfo(cpu);
+    define_arm_cp_regs(cpu, cortex_a78_reginfo);
 #ifndef CONFIG_USER_ONLY
     define_arm_cp_regs(cpu, dsu_cp_reginfo);
 #endif
