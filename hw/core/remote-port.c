fa9332d20a67d14c8381025627dbbff3cc714d27
Frederic Konrad <fkonrad@amd.com>
2022-12-23 16:43:24 +0100

    remote-port: don't use unix specific nonblocking function
    Since the merge, g_unix_set_fd_nonblocking() is used to set the fd in non
    blocking mode.  This works well for Unix, but not for MinGW so don't use
    it at all and use the target agnostic qemu_socket_set_nonblock() function
    instead.
    
    Signed-off-by: Frederic Konrad <fkonrad@amd.com>
    Signed-off-by: Francisco Iglesias <francisco.iglesias@amd.com>
    [FK: 260326 - fixed against qemu_socket_set_nonblock removal
                  (8cb17f9c36f util: drop qemu_socket_set_nonblock())]
    Signed-off-by: Frederic Konrad <fkonrad@amd.com>

--- hw/core/remote-port.c
+++ hw/core/remote-port.c
@@ -705,7 +705,6 @@ static void rp_realize(DeviceState *dev, Error **errp)
 {
     RemotePort *s = REMOTE_PORT(dev);
     int r;
-    g_autoptr(GError) err = NULL;
 
     s->prefix = object_get_canonical_path(OBJECT(dev));
 
@@ -814,8 +813,9 @@ static void rp_realize(DeviceState *dev, Error **errp)
             }
         }
 
-        g_unix_set_fd_nonblocking(s->event.pipe.read, true, &err);
-        g_assert_no_error(err);
+        if (!qemu_set_blocking(s->event.pipe.read, false, errp)) {
+            return;
+        }
         qemu_set_fd_handler(s->event.pipe.read, rp_event_read, NULL, s);
     }
 #else
@@ -824,8 +824,9 @@ static void rp_realize(DeviceState *dev, Error **errp)
                     s->prefix);
         exit(EXIT_FAILURE);
     }
-    g_unix_set_fd_nonblocking(s->event.pipe.read, true, &err);
-    g_assert_no_error(err);
+    if (!qemu_set_blocking(s->event.pipe.read, false, errp)) {
+        return;
+    }
     qemu_set_fd_handler(s->event.pipe.read, rp_event_read, NULL, s);
 #endif
 
