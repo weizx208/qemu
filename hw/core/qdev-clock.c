c96691fbaa8d185f50102661e2b40ea563a9de4e
Luc Michel <luc.michel@amd.com>
2024-10-30 17:19:10 +0100

    hw/core/qdev-clock: return NULL for non-existant clock input/output
    Return NULL in qdev_get_clock_input/output instead of aborting. This
    allows to probe clocks on a device.
    
    Signed-off-by: Luc Michel <luc.michel@amd.com>
    Signed-off-by: Francisco Iglesias <francisco.iglesias@amd.com>

--- hw/core/qdev-clock.c
+++ hw/core/qdev-clock.c
@@ -127,9 +127,7 @@ Clock *qdev_get_clock_in(DeviceState *dev, const char *name)
 
     ncl = qdev_get_clocklist(dev, name);
     if (!ncl) {
-        error_report("Can not find clock-in '%s' for device type '%s'",
-                     name, object_get_typename(OBJECT(dev)));
-        abort();
+        return NULL;
     }
     assert(!ncl->output);
 
@@ -144,9 +142,7 @@ Clock *qdev_get_clock_out(DeviceState *dev, const char *name)
 
     ncl = qdev_get_clocklist(dev, name);
     if (!ncl) {
-        error_report("Can not find clock-out '%s' for device type '%s'",
-                     name, object_get_typename(OBJECT(dev)));
-        abort();
+        return NULL;
     }
     assert(ncl->output);
 
@@ -179,6 +175,15 @@ Clock *qdev_alias_clock(DeviceState *dev, const char *name,
 
 void qdev_connect_clock_in(DeviceState *dev, const char *name, Clock *source)
 {
+    Clock *in;
+
     assert(!dev->realized);
-    clock_set_source(qdev_get_clock_in(dev, name), source);
+
+    in = qdev_get_clock_in(dev, name);
+
+    if (in == NULL) {
+        return;
+    }
+
+    clock_set_source(in, source);
 }
