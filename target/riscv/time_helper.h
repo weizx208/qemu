0c9beb8b936dc7e9b7097f9fecfbbf2ecc406c99
Luc Michel <luc.michel@amd.com>
2025-11-06 15:50:33 +0100

    target/riscv: add the RISCVCPUTimeSrcIf interface
    Add the RISCVCPUTimeSrcIf QOM interface to the RISC-V target. This
    interface aims at replacing the existing env->rdtime_fn callback in the
    RISC-V CPU env. It allows to query the current number of ticks, and the
    tick frequency.
    
    Signed-off-by: Luc Michel <luc.michel@amd.com>

--- target/riscv/time_helper.h
+++ target/riscv/time_helper.h
@@ -27,4 +27,20 @@ void riscv_timer_write_timecmp(CPURISCVState *env, QEMUTimer *timer,
                                uint32_t timer_irq);
 void riscv_timer_init(RISCVCPU *cpu);
 
+static inline uint64_t riscv_cpu_time_src_get_ticks(RISCVCPUTimeSrcIf *src)
+{
+    RISCVCPUTimeSrcIfClass *rctsc = RISCV_CPU_TIME_SRC_IF_GET_CLASS(src);
+
+    g_assert(rctsc->get_ticks != NULL);
+    return rctsc->get_ticks(src);
+}
+
+static inline uint32_t riscv_cpu_time_src_get_tick_freq(RISCVCPUTimeSrcIf *src)
+{
+    RISCVCPUTimeSrcIfClass *rctsc = RISCV_CPU_TIME_SRC_IF_GET_CLASS(src);
+
+    g_assert(rctsc->get_tick_freq != NULL);
+    return rctsc->get_tick_freq(src);
+}
+
 #endif
