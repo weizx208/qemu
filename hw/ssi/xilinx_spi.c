b370d61fa0ccea1a89a72eecb867cd74409a0826
Frederic Konrad <fkonrad@amd.com>
2026-02-09 12:30:07 +0100

    hw/ssi/xilinx_spi.c: fix R_SPICR reset value.
    This has been extracted from:
      02d2f0203dd PetaLinux QEMU v2015.2
    
    Signed-off-by: Frederic Konrad <fkonrad@amd.com>

--- hw/ssi/xilinx_spi.c
+++ hw/ssi/xilinx_spi.c
@@ -63,6 +63,7 @@
 #define R_SPICR_TXFF_RST     (1 << 5)
 #define R_SPICR_RXFF_RST     (1 << 6)
 #define R_SPICR_MTI          (1 << 8)
+#define R_SPICR_MANUAL_SS_EN (1 << 7)
 
 #define R_SPISR     (0x64 / 4)
 #define SR_TX_FULL    (1 << 3)
@@ -162,6 +163,7 @@ static void xlx_spi_do_reset(XilinxSPI *s)
     s->regs[R_SPICR] = R_SPICR_MTI;
     xlx_spi_update_irq(s);
     xlx_spi_update_cs(s);
+    s->regs[R_SPICR] = R_SPICR_MTI | R_SPICR_MANUAL_SS_EN;
 }
 
 static void xlx_spi_reset(DeviceState *d)
