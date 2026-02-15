5c4eb9098137ac59c245e01bbc7ba7a8bd83f993
Frederic Konrad <fkonrad@amd.com>
2026-02-15 01:31:07 +0100

    hw/block/m25p80: Add support for mt35xu01gbba
    This has been extracted from:
      edd09f92c71 m25p80: Reset to v2.11.0 mainline
    
    Signed-off-by: Frederic Konrad <fkonrad@amd.com>

--- hw/block/m25p80.c
+++ hw/block/m25p80.c
@@ -260,6 +260,7 @@ static const FlashPartInfo known_devices[] = {
       .sfdp_read = m25p80_sfdp_n25q256a },
     { INFO("n25q512a11",  0x20bb20,      0,  64 << 10, 1024, ER_4K) },
     { INFO("n25q512a13",  0x20ba20,      0,  64 << 10, 1024, ER_4K) },
+    { INFO("mt35xu01gbba", 0x2c5b1b,     0, 128 << 10, 1024, ER_4K) },
     { INFO("n25q128",     0x20ba18,      0,  64 << 10, 256, 0) },
     { INFO("n25q256a",    0x20ba19,      0,  64 << 10, 512,
            ER_4K | HAS_SR_BP3_BIT6 | HAS_SR_TB),
