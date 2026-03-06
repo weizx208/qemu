66bebfd4aad471420f72d14cff9d5b65d1531103
Luc Michel <luc.michel@amd.com>
2026-03-06 17:42:10 +0100

    hwdtb: block device binding: add the legacy binding method
    The legacy method relies on the `drive-index' property on the device
    node, and on several heuristics based on the device type and
    configuration. Remaining users of this should be converted to the modern
    way and this code removed.
    
    Signed-off-by: Luc Michel <luc.michel@amd.com>

--- hwdtb/blockdev.c
+++ hwdtb/blockdev.c
@@ -10,6 +10,9 @@
 #include "qemu/hwdtb.h"
 #include "system/blockdev.h"
 #include "hw/qdev-properties.h"
+#include "hw/ssi/ssi.h"
+#include "hw/nvram/xlnx-bbram.h"
+#include "hw/nvram/xlnx-efuse.h"
 #include "qapi/error.h"
 #include "trace.h"
 
@@ -41,6 +44,106 @@ static bool node_attach_block_dev_node_name(HwDtbNode *node)
     return true;
 }
 
+/*
+ * -- Legacy --
+ * Legacy heuristic assiociating a device type to a block interface type.
+ */
+static BlockInterfaceType node_get_block_iface_type(HwDtbNode *node)
+{
+    if (HWDTB_NODE_AS(node, XLNX_BBRAM)) {
+        return IF_PFLASH;
+    }
+
+    if (HWDTB_NODE_AS(node, XLNX_EFUSE)) {
+        return IF_PFLASH;
+    }
+
+    if (HWDTB_NODE_AS(node, SSI_PERIPHERAL)) {
+        return IF_MTD;
+    }
+
+    /* No match */
+    return IF_NONE;
+}
+
+/*
+ * -- Legacy --
+ * Legacy heuristic associating a device type to a default drive index.
+ *
+ * Some dtbs don't have the drive-index property. The legacy code uses some
+ * tricks by reading device properties to deduce the machine type, and then
+ * attach the drive at correct index to the device.
+ *
+ * Those are reproduced here for backward compatibility.
+ */
+static uint32_t node_get_block_default_drive_idx(HwDtbNode *node)
+{
+    if (HWDTB_NODE_AS(node, XLNX_BBRAM)) {
+        bool is_versal;
+
+        /* not versal -> zynqmp */
+        is_versal = object_property_get_uint(hwdtb_get_obj(node), "crc-zpads",
+                                             &error_abort) == 0;
+
+        return is_versal ? 0 : 2;
+    }
+
+    if (HWDTB_NODE_AS(node, XLNX_EFUSE)) {
+        bool is_versal;
+
+        /* not versal -> zynqmp */
+        is_versal = object_property_get_uint(hwdtb_get_obj(node), "efuse-size",
+                                             &error_abort) > 2048;
+
+        return is_versal ? 1 : 3;
+    }
+
+    return UINT32_MAX;
+}
+
+static DriveInfo *node_get_drive_info(HwDtbNode *node, BlockInterfaceType type)
+{
+    uint32_t index;
+
+    if (hwdtb_node_get_prop_uint32(node, "drive-index", &index)) {
+        return drive_get_by_index(type, index);
+    } else {
+        return drive_get_next(type);
+    }
+}
+
+static bool node_attach_interface_index(HwDtbNode *node)
+{
+    BlockInterfaceType iface;
+    uint32_t default_idx;
+    DriveInfo *info;
+    BlockBackend *bb;
+
+    iface = node_get_block_iface_type(node);
+
+    if (iface == IF_NONE) {
+        return false;
+    }
+
+    default_idx = node_get_block_default_drive_idx(node);
+
+    if (default_idx != UINT32_MAX) {
+        info = drive_get_by_index(iface, default_idx);
+    } else {
+        info = node_get_drive_info(node, iface);
+    }
+
+    if (info == NULL) {
+        return false;
+    }
+
+    bb = blk_by_legacy_dinfo(info);
+    qdev_prop_set_drive(DEVICE(hwdtb_get_obj(node)), "drive", bb);
+    trace_hwdtb_node_set_prop_drive(node->path, "drive", blk_name(bb));
+
+    return true;
+}
+
 static void node_attach_block_dev(HwDtbNode *node)
 {
     if (!HWDTB_NODE_AS(node, DEVICE)) {
@@ -65,6 +168,12 @@ static void node_attach_block_dev(HwDtbNode *node)
     if (node_attach_block_dev_node_name(node)) {
         return;
     }
+
+    /*
+     * -- Legacy --
+     * Try the legacy BlockInterfaceType/index method.
+     */
+    node_attach_interface_index(node);
 }
 
 void hwdtb_attach_block_devs(HwDtb *hwdtb)
