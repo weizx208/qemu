weizx208/qemu

commit dddca71bbc506b668f3a036082a320fbc4b56eaa

hw/misc/xlnx-psxc-lpx-slcr: implement the TYPE_ARM_LINUX_BOOT_IF iface
Implement the TYPE_ARM_LINUX_BOOT_IF interface. In case of a direct
Linux kernel boot, set the APUs to off at reset.

When directly booting a kernel, QEMU is handling the PSCI calls made by
Linux to EL3 to start secondary CPUs. This PSCI emulation expects the
cores to be off when the request is made. This commit ensures this.

This is kind of a hack really. It works because the power GPIO on the
cores is currently implemented by altering the PSCI state of the CPU.
Setting the GPIO off will put the CPU in PSCI_OFF state. If the power
GPIO was implemented in a more proper way, like really preventing QEMU
from executing the CPU, independently of its PSCI state, this would
break. When the PSCI emulation turns the core on, we end up in a
inconsistent situation where the core is running but its corresponding
power GPIO is off. This does not really matter since booting a kernel
directly implies that we don't have any low level software taking care
of the power states and such. This should be kept in mind however if we
happen to modify the way the power GPIO is implemented on the CPUs.

Signed-off-by: Luc Michel <luc.michel@amd.com>
Signed-off-by: Francisco Iglesias <francisco.iglesias@amd.com>

hw/misc/xlnx-psxc-lpx-slcr.c
@@ -31,6 +31,7 @@
 #include "qemu/log.h"
 #include "migration/vmstate.h"
 #include "hw/qdev-properties.h"
+#include "hw/arm/linux-boot-if.h"
 #include "hw/fdt_generic_util.h"
 #include "hw/misc/xlnx-psxc-lpx-slcr.h"
 #include "trace.h"
@@ -7763,9 +7764,15 @@ static const MemoryRegionOps psxc_lpx_slcr_ops = {
     },
 };
 
-static void core_pwr_ctrl_reset(XlnxPsxcLpxSlcrCorePowerCtrl *pwr_ctrl)
+static void core_pwr_ctrl_reset(XlnxPsxcLpxSlcrCorePowerCtrl *pwr_ctrl,
+                                bool linux_boot)
 {
-    pwr_ctrl->reg0 = APU0_CORE0_PWR_CNTRL_REG0_RESET_VAL;
+    if (linux_boot) {
+        pwr_ctrl->reg0 = 0; /* reset with the core powered off */
+    } else {
+        pwr_ctrl->reg0 = APU0_CORE0_PWR_CNTRL_REG0_RESET_VAL;
+    }
+
     pwr_ctrl->reg1 = APU0_CORE0_PWR_CNTRL_REG1_RESET_VAL;
     pwr_ctrl->reg2 = APU0_CORE0_PWR_CNTRL_REG2_RESET_VAL;
     pwr_ctrl->wprot = APU0_CORE0_PWR_CNTRL_WPROT_RESET_VAL;
@@ -7813,7 +7820,7 @@ static void psxc_lpx_slcr_reset_enter(Object *obj, ResetType type)
     }
 
     for (i = 0; i < ARRAY_SIZE(s->core_pwr); i++) {
-        core_pwr_ctrl_reset(&s->core_pwr[i]);
+        core_pwr_ctrl_reset(&s->core_pwr[i], s->linux_boot);
     }
 }
 
@@ -7909,6 +7916,14 @@ static void psxc_lpx_slcr_realize(DeviceState *dev, Error **errp)
             }
         }
     }
+}
+
+static void psxc_lpx_slcr_linux_boot_if_init(ARMLinuxBootIf *obj,
+                                             bool secure_boot)
+{
+    XlnxPsxcLpxSlcr *s = XILINX_PSXC_LPX_SLCR(obj);
+
+    s->linux_boot = true;
 }
 
 static void psxc_lpx_slcr_init(Object *obj)
@@ -8046,6 +8061,7 @@ static void psxc_lpx_slcr_class_init(ObjectClass *klass, void *data)
     ResettableClass *rc = RESETTABLE_CLASS(klass);
     DeviceClass *dc = DEVICE_CLASS(klass);
     FDTGenericGPIOClass *fggc = FDT_GENERIC_GPIO_CLASS(klass);
+    ARMLinuxBootIfClass *albifc = ARM_LINUX_BOOT_IF_CLASS(klass);
 
     dc->realize = psxc_lpx_slcr_realize;
     dc->vmsd = &vmstate_psxc_lpx_slcr;
@@ -8053,6 +8069,7 @@ static void psxc_lpx_slcr_class_init(ObjectClass *klass, void *data)
     rc->phases.hold = psxc_lpx_slcr_reset_hold;
     rc->phases.exit = psxc_lpx_slcr_reset_exit;
     device_class_set_props(dc, psxc_lpx_slcr_properties);
+    albifc->arm_linux_init = psxc_lpx_slcr_linux_boot_if_init;
     fggc->controller_gpios = psxc_lpx_slcr_gpios;
 }
 
@@ -8063,6 +8080,7 @@ static const TypeInfo psxc_lpx_slcr_info = {
     .class_init    = psxc_lpx_slcr_class_init,
     .instance_init = psxc_lpx_slcr_init,
     .interfaces    = (InterfaceInfo[]) {
+        { TYPE_ARM_LINUX_BOOT_IF },
         { TYPE_FDT_GENERIC_GPIO },
         { }
     },

include/hw/misc/xlnx-psxc-lpx-slcr.h
@@ -88,6 +88,7 @@ typedef struct XlnxPsxcLpxSlcr {
     XlnxPsxcLpxSlcrIrq rpu_pcil_wfi_irq;
 
     uint32_t num_rpu;
+    bool linux_boot;
 } XlnxPsxcLpxSlcr;
 
 #endif

commit e0c3d247adfbd23eb313806073e85300085182e8

hw/misc/xlnx-psxc-lpx-slcr: fix direct linux boot
When booting in direct linux mode, after a reset, the first APU core
must be powered up and the secondary ones down. Currently, all the cores
are down. This commit fixes this.

Fixes: dddca71bbc50 (hw/misc/xlnx-psxc-lpx-slcr: implement the TYPE_ARM_LINUX_BOOT_IF iface)
Signed-off-by: Luc Michel <luc.michel@amd.com>
Signed-off-by: Francisco Iglesias <francisco.iglesias@amd.com>

hw/misc/xlnx-psxc-lpx-slcr.c
@@ -8055,9 +8055,9 @@ static const MemoryRegionOps psxc_lpx_slcr_ops = {
 };
 
 static void core_pwr_ctrl_reset(XlnxPsxcLpxSlcrCorePowerCtrl *pwr_ctrl,
-                                bool linux_boot)
+                                bool start_off)
 {
-    if (linux_boot) {
+    if (start_off) {
         pwr_ctrl->reg0 = 0; /* reset with the core powered off */
     } else {
         pwr_ctrl->reg0 = APU0_CORE0_PWR_CNTRL_REG0_RESET_VAL;
@@ -8112,7 +8112,8 @@ static void psxc_lpx_slcr_reset_enter(Object *obj, ResetType type)
     }
 
     for (i = 0; i < ARRAY_SIZE(s->core_pwr); i++) {
-        core_pwr_ctrl_reset(&s->core_pwr[i], s->linux_boot);
+        /* when direct Linux booting, start with core 0 on and the rest off */
+        core_pwr_ctrl_reset(&s->core_pwr[i], i ? s->linux_boot : false);
     }
 }
 