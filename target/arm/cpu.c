weizx208/qemu

commit eb7554f234f37b86548f6d7cb8098ec2281ec04f

hw/arm/pchannel: add the state change notifier
Add a state change notifier to the ARMPChannelIf interface. This allows
the interface user to register a notifier that will be called when the
CPU current PACTIVE state changes.

Signed-off-by: Luc Michel <luc.michel@amd.com>

target/arm/cpu.c
@@ -599,6 +599,7 @@ static void arm_cpu_reset_hold(Object *obj)
     }
 
     qemu_set_irq(cpu->wfi, false);
+    arm_cpu_notify_pactive_change(cpu);
 
 #ifndef CONFIG_USER_ONLY
     if (cpu->env.memattr_ns) {
@@ -623,6 +624,8 @@ static void arm_cpu_reset_hold(Object *obj)
 #ifndef CONFIG_USER_ONLY
     cpu_halt_update(s);
 #endif
+
+    arm_cpu_notify_pactive_change(cpu);
 }
 
 void arm_emulate_firmware_reset(CPUState *cpustate, int target_el)
@@ -1436,6 +1439,14 @@ static bool pchannel_request_state_change_unimp(ARMPChannelIf *obj,
 {
     return false;
 }
+
+static void cpu_pchannel_register_state_change_notifier(ARMPChannelIf *obj,
+                                                        Notifier *notifier)
+{
+    ARMCPU *cpu = ARM_CPU(obj);
+
+    notifier_list_add(&cpu->pchannel_state_change, notifier);
+}
 #endif
 
 static void arm_cpu_initfn(Object *obj)
@@ -1507,6 +1518,10 @@ static void arm_cpu_initfn(Object *obj)
                              (Object **)&cpu->env.memattr_s,
                              qdev_prop_allow_set_link_before_realize,
                              OBJ_PROP_LINK_STRONG);
+#endif
+
+#ifndef CONFIG_USER_ONLY
+    notifier_list_init(&cpu->pchannel_state_change);
 #endif
 }
 
@@ -2734,6 +2749,8 @@ static void arm_cpu_class_init(ObjectClass *oc, void *data)
 #ifndef CONFIG_USER_ONLY
     apcic->get_current_state = pchannel_get_current_state_unimp;
     apcic->request_state_change = pchannel_request_state_change_unimp;
+    apcic->register_state_change_notifier =
+        cpu_pchannel_register_state_change_notifier;
 #endif
 }
 

include/hw/arm/pchannel.h
@@ -48,6 +48,14 @@ struct ARMPChannelIfClass {
      * Returns: the device specific P-Channel state
      */
     uint32_t (*get_current_state)(ARMPChannelIf *obj);
+
+    /**
+     * register_state_change_notifier
+     *
+     * Register a notifier called when the P-Channel state changes
+     */
+    void (*register_state_change_notifier)(ARMPChannelIf *obj,
+                                           Notifier *notifier);
 };
 
 static inline bool pchannel_request_state_change(ARMPChannelIf *obj,
@@ -63,6 +71,16 @@ static inline uint32_t pchannel_get_current_state(ARMPChannelIf *obj)
     ARMPChannelIfClass *klass = ARM_PCHANNEL_IF_GET_CLASS(obj);
 
     return klass->get_current_state(obj);
+}
+
+static inline void pchannel_register_state_change_notifier(ARMPChannelIf *obj,
+                                                           Notifier *notifier)
+{
+    ARMPChannelIfClass *klass = ARM_PCHANNEL_IF_GET_CLASS(obj);
+
+    if (klass->register_state_change_notifier) {
+        klass->register_state_change_notifier(obj, notifier);
+    }
 }
 
 struct ARMPChannelDummyState {

target/arm/arm-powerctl.c
@@ -90,6 +90,7 @@ static void arm_set_cpu_on_async_work(CPUState *target_cpu_state,
     /* Finally set the power status */
     assert(qemu_mutex_iothread_locked());
     target_cpu->power_state = PSCI_ON;
+    arm_cpu_notify_pactive_change(target_cpu);
 }
 
 int arm_set_cpu_on(uint64_t cpuid, uint64_t entry, uint64_t context_id,
@@ -198,6 +199,7 @@ static void arm_set_cpu_on_and_reset_async_work(CPUState *target_cpu_state,
     /* Finally set the power status */
     assert(qemu_mutex_iothread_locked());
     target_cpu->power_state = PSCI_ON;
+    arm_cpu_notify_pactive_change(target_cpu);
 }
 
 int arm_set_cpu_on_and_reset(uint64_t cpuid)
@@ -251,6 +253,7 @@ static void arm_set_cpu_off_async_work(CPUState *target_cpu_state,
     target_cpu->power_state = PSCI_OFF;
     target_cpu_state->halted = 1;
     target_cpu_state->exception_index = EXCP_HLT;
+    arm_cpu_notify_pactive_change(target_cpu);
 }
 
 int arm_set_cpu_off(uint64_t cpuid)

target/arm/cpu.h
@@ -1168,6 +1168,10 @@ struct ArchCPU {
 
     /* Generic timer counter frequency, in Hz */
     uint64_t gt_cntfrq_hz;
+
+#ifndef CONFIG_USER_ONLY
+    NotifierList pchannel_state_change;
+#endif
 };
 
 typedef struct ARMCPUInfo {
@@ -3502,5 +3506,12 @@ static inline target_ulong cpu_untagged_addr(CPUState *cs, target_ulong x)
     return x;
 }
 #endif
+
+static inline void arm_cpu_notify_pactive_change(ARMCPU *cpu)
+{
+#ifndef CONFIG_USER_ONLY
+    notifier_list_notify(&cpu->pchannel_state_change, cpu);
+#endif
+}
 
 #endif

commit 345b79710daf34a0effffe2d044aa0aebe2b4c5b

target/arm/cpu: fix arm_cpu_notify_pactive_change call-site
The arm_cpu_notify_pactive_change function was mistakenly called twice
in the arm_cpu_reset_hold_function, and never called in
arm_cpu_exec_interrupt when the CPU goes out of WFI.

Fixes: eb7554f234f3
Signed-off-by: Luc Michel <luc.michel@amd.com>
Signed-off-by: Francisco Iglesias <francisco.iglesias@amd.com>

target/arm/cpu.c
@@ -599,7 +599,6 @@ static void arm_cpu_reset_hold(Object *obj)
     }
 
     qemu_set_irq(cpu->wfi, false);
-    arm_cpu_notify_pactive_change(cpu);
 
 #ifndef CONFIG_USER_ONLY
     if (cpu->env.memattr_ns) {
@@ -868,6 +867,7 @@ static bool arm_cpu_exec_interrupt(CPUState *cs, int interrupt_request)
     uint32_t excp_idx;
 
     qemu_irq_lower(ARM_CPU(cs)->wfi);
+    arm_cpu_notify_pactive_change(ARM_CPU(cs));
 
     /* The prioritization of interrupts is IMPLEMENTATION DEFINED. */
 